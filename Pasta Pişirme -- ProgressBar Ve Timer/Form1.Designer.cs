69c8ab68ca11a43b93d6512b650dd52e99d3361b
havvacapar <havvacapar35@gmail.com>
2023-11-15 13:55:24 +0300

    Add project files.

---
+++ Pasta Pişirme -- ProgressBar Ve Timer/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Pasta_Pişirme____ProgressBar_Ve_Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Un ve Yumurta Karıştırılıyor...";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 93);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(718, 56);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(13, 215);
+            this.progressBar2.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(718, 56);
+            this.progressBar2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 171);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Çırpma İşlemi Yapılıyor...";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(13, 349);
+            this.progressBar3.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(718, 56);
+            this.progressBar3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 304);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(409, 35);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Malzemeler Ekleniyor ve Karıştırılıyor...";
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(13, 470);
+            this.progressBar4.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(718, 56);
+            this.progressBar4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 425);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(188, 35);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Pasta Pişiriliyor...";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 557);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 46);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(276, 563);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 35);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(744, 615);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Pasta Pişirme -- ProgressBar Ve Timer/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pasta_Pişirme____ProgressBar_Ve_Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var s = Convert.ToInt32(label5.Text);
+            progressBar1.Value += 10;
+            s += 5;
+            if (progressBar1.Value % 20 == 10)
+            {
+                label1.BackColor = Color.Red;
+
+            }
+            if (progressBar1.Value % 20 == 0)
+            {
+                label1.BackColor = Color.White;
+            }
+            if (progressBar1.Value == 100)
+            {
+                timer1.Stop();
+                timer2.Start();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            progressBar2.Value += 10;
+            if (progressBar2.Value % 20 == 10)
+            {
+                label2.BackColor = Color.Red;
+            }
+            if (progressBar2.Value % 20 == 0)
+            {
+                label2.BackColor = Color.White;
+            }
+            if (progressBar2.Value == 100)
+            {
+                timer2.Stop();
+                timer3.Start();
+            }
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            
+            progressBar3.Value += 10;
+            if (progressBar3.Value % 20 == 10)
+            {
+                label3.BackColor = Color.Red;
+                
+            }
+            if (progressBar3.Value % 20 == 0)
+            {
+                label3.BackColor = Color.White;
+            }
+            if (progressBar3.Value == 100)
+            {
+                timer3.Stop();
+                timer4.Start();
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            progressBar4.Value += 10;
+            if (progressBar4.Value % 20 == 10)
+            {
+                label4.BackColor = Color.Red;
+            }
+            if (progressBar4.Value % 20 == 0)
+            {
+                label4.BackColor = Color.White;
+            }
+            if (progressBar4.Value == 100)
+            {
+                timer4.Stop();
+                MessageBox.Show("Pastanız Hazır.Afiyet Olsun :)");
+            }
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label5.Text = " ";
+        }
+    }
+}
